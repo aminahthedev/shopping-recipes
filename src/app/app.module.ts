5a0c76b222bb57ebaa7a8644c79ef858dc99c3ce
Aminah Burch <a.j.burch11@gmail.com>
2018-03-05 18:31:36 -0600

    Created the initial app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditorComponent } from './shopping-list/shopping-list-editor/shopping-list-editor.component';
+import { RecipeBookComponent } from './recipes/recipe-book/recipe-book.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-book/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditorComponent,
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
